7bfece7d301ffc674b53672531ae51529944b142
Peter Toi <peter@petertoi.com>
2019-06-05 10:36:38 -0400

    Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,12 @@
 <footer class="content-info">
   <div class="container">
     <div class="row">
-      <div class="col">Privacy</div>
-      <div class="col text-right">Colophon</div>
+      <div class="col text-center">
+        <ul class="nav">
+          <li><a href="#">Colophon</a></li> |
+          <li><a href="#">Privacy</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </footer>
