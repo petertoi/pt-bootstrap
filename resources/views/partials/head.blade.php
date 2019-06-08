48ac1ed6f23ff8eab9f0a12935ea6879f1eb2b36
Peter Toi <peter@petertoi.com>
2019-06-07 22:04:20 -0400

    Let's go live. Now with GA.

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,4 +3,16 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php wp_head() @endphp
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141729463-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {dataLayer.push( arguments );}
+
+    gtag( 'js', new Date() );
+
+    gtag( 'config', 'UA-141729463-2' );
+  </script>
+
 </head>
